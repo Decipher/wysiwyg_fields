d0a28790052b8d9bd182c001e326644fe73615fb
Decipher <stu@rtclark.net>
2011-03-21 08:08:57 +1100

    Added Widget settings bypass hook.

--- wysiwyg_fields.api.php
+++ wysiwyg_fields.api.php
@@ -11,7 +11,7 @@ function hook_wysiwyg_fields_add_js($type) {
   switch ($type) {
     case 'node_form':
       foreach (wysiwyg_profile_load_all() as $profile) {
-        if (isset($profile->editor) && file_exists($js = drupal_get_path('module', 'wysiwyg_fields') . "/scripts/wysiwyg.{$profile->editor}.js")) {
+        if (isset($profile->editor) && file_exists($js = drupal_get_path('module', 'MYMODULE') . "/scripts/wysiwyg.{$profile->editor}.js")) {
           drupal_add_js($js);
         }
       }
@@ -23,8 +23,8 @@ function hook_wysiwyg_fields_add_js($type) {
  * Implements hook_wysiwyg_fields_elements_alter().
  */
 function hook_wysiwyg_fields_elements_alter($elements) {
-  $elements['date_select'] = array(
-    '#process' => array('_date_wysiwyg_fields_element_alter_process'),
+  $elements['MYMODULE_WIDGET'] = array(
+    '#process' => array('_MYMODULE_wysiwyg_fields_element_alter_process'),
   );
 }
 
@@ -33,7 +33,7 @@ function hook_wysiwyg_fields_elements_alter($elements) {
  */
 function hook_wysiwyg_fields_icons() {
   return array(
-    drupal_get_path('module', 'wysiwyg_fields') . '/images/icons',
+    drupal_get_path('module', 'MYMODULE') . '/images/icons',
   );
 }
 
@@ -42,9 +42,19 @@ function hook_wysiwyg_fields_icons() {
  */
 function hook_wysiwyg_fields_theme_bypass($widget_type) {
   switch ($widget_type) {
-    case 'date_select':
+    case 'MYMODULE_WIDGET':
       return TRUE;
   }
+  return FALSE;
+}
 
+/**
+ * Implements hook_wysiwyg_fields_widget_bypass().
+ */
+function hook_wysiwyg_fields_widget_bypass($widget) {
+  switch ($widget['module']) {
+    case 'MYMODULE':
+      return TRUE;
+  }
   return FALSE;
 }
